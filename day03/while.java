e97177d47bb84b4ded307636d815f1f61a754c3f
shaleeni <mshaleeni07@gmail.com>
2026-07-22 19:45:19 +0530

    Add While class to print numbers 1 to 10
    Implement a while loop to print numbers from 1 to 10.

---
+++ day03/while.java
@@ -0,0 +1,12 @@
+public class While {
+    //Print numbers from 1 to 10 using a while loop.
+    public static void main(String args[])
+    {
+
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        }
+    }
