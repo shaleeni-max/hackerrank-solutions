f8dfa791928a56e7e83902dddcffb0ca9c193347
shaleeni <mshaleeni07@gmail.com>
2026-07-22 19:47:09 +0530

    Add Whilemult class to print multiplication table of 7

---
+++ day03/whilemult.java
@@ -0,0 +1,11 @@
+public class Whilemult {
+    public static void main(String args[])
+    {
+        //Print the multiplication table of 7.
+        int i=1;
+        while(i<=10){
+            System.out.println(i*7);
+            i++;
+        }
+    }
+}
